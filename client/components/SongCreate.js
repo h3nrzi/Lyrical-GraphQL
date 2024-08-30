5b535e9875f6e434956c30cd4810c49739ac1015
Hossein Rezaei <rezaeig22@gmail.com>
2024-08-30 14:45:46 +0330

    Create Song Component

---
+++ client/components/SongCreate.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+class SongCreate extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { title: "" };
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Create a New Song</h3>
+        <form>
+          <label>Song Title:</label>
+          <input value={this.state.title} onChange={e => this.setState({ title: e.target.value })} />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SongCreate;
