63dae196dcba6867b586664dbecf41ed559ccf63
Hossein Rezaei <rezaeig22@gmail.com>
2024-08-30 12:25:09 +0330

    gql queries

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,9 +1,21 @@
 import React, { Component } from "react";
+import gql from "graphql-tag";
 
 class SongList extends Component {
   render() {
     return <div>SongList</div>;
   }
 }
+
+const query = gql`
+  {
+    songs {
+      id
+      title
+    }
+  }
+`;
+
+console.log(query);
 
 export default SongList;
