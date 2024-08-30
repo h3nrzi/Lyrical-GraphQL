535d29972ef183a7f7eb094e86308f19d996db17
Hossein Rezaei <rezaeig22@gmail.com>
2024-08-30 21:45:12 +0330

    Converting SongList Class Component to Functional

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -3,30 +3,26 @@ import gql from "graphql-tag";
 import { graphql } from "react-apollo";
 import { Link } from "react-router";
 
-class SongList extends React.Component {
-  renderSongs() {
-    return this.props.data.songs.map(song => {
-      return (
-        <li className="collection-item" key={song.id}>
-          {song.title}
-        </li>
-      );
-    });
-  }
+const SongList = ({ data }) => {
+  if (data.loading) return <div>Loading...</div>;
 
-  render() {
-    if (this.props.data.loading) return <div>Loading...</div>;
+  return (
+    <div>
+      <ul className="collection">
+        {data.songs.map(song => (
+          <Song key={song.id} song={song} />
+        ))}
+      </ul>
+      <Link to="/songs/new" className="btn-floating btn-large red right" onlyActiveOnIndex>
+        <i className="material-icons">add</i>
+      </Link>
+    </div>
+  );
+};
 
-    return (
-      <div>
-        <ul className="collection">{this.renderSongs()}</ul>
-        <Link to="/songs/new" className="btn-floating btn-large red right" onlyActiveOnIndex>
-          <i className="material-icons">add</i>
-        </Link>
-      </div>
-    );
-  }
-}
+const Song = ({ song }) => {
+  return <li className="collection-item">{song.title}</li>;
+};
 
 const query = gql`
   {
